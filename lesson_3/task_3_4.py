1a46cef351dc9e4518846b306a6bd37d411a3182
Vasiliy_S <vasey@yandex.ru>
2020-04-21 20:44:49 +0300

    Lesson #3 homework. Some fixes

--- lesson_3/task_3_4.py
+++ lesson_3/task_3_4.py
@@ -10,17 +10,17 @@
 def my_pow_func_version_1(x: float, y: int):
     try:
         result = x ** abs(y)
-        print(result)
+        print(result if y >= 0 else 1 / result)
     except TypeError:
         print("Error type arguments")
 
 
 def my_pow_func_version_2(x: float, y: int):
     try:
-        result = x
-        for i in range(1, abs(y)):
+        result = 1
+        for i in range(abs(y)):
             result *= x
-        print(result)
+        print(result if y >= 0 else 1 / result)
     except TypeError:
         print("Error type arguments")
 
@@ -28,7 +28,7 @@ def my_pow_func_version_2(x: float, y: int):
 # test
 if __name__ == '__main__':
     number = 10.0
-    degree = -2
+    degree = 0
     print(f"number = {number}, degree = {degree}")
     print("Result of my_pow_func_version_1:")
     my_pow_func_version_1(number, degree)
